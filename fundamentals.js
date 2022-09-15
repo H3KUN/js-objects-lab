9b21f6005a2716dfc152901fc0e636e857ed3917
Hiroto Iwata <hiroto1610@outlook.com>
2022-09-15 17:11:23 +0100

    Loop constructs.

--- fundamentals.js
+++ fundamentals.js
@@ -2,24 +2,31 @@ const aCar = {
     owner : "Joe Bloggs",
     address : '3 Walkers Lane',
     previous_owners: [
-        {name:"Pat Smith",adress:"Main Street"},
-        {name:"Sheila Dwyer",adress:"High Street"}
+        {name:"Pat Smith",address:"Main Street"},
+        {name:"Sheila Dwyer",address:"High Street"}
     ],
     type : {
         make : 'Toyota',
         model : 'Corolla',
         cc : '1.8'
     },
+    features : ['Parking assist', 'Alarm', 'Tow-bar'],
     registration : '201WD1058',
-  };
-  aCar.mileage = 10000;
-  aCar.colour = {
-        exterior : {
+};
+aCar.mileage = 10000;
+aCar.colour = {
+    exterior : {
         colour : 'red'
     },
     interior : {
         texture : 'leather',
         shade : 'cream'
     }
-  };
-  console.log('First owner : ' + aCar.previous_owners[0].name + '-'+ aCar.previous_owners[0].adress)
+};
+//console.log('First owner : ' + aCar.previous_owners[0].name + '-'+ aCar.previous_owners[0].adress)
+for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+    console.log(aCar.previous_owners[i].name+' - '+aCar.previous_owners[i].address) ;
+} 
+for (let p in aCar.type)  {
+    console.log(p.toUpperCase() + ' = ' + aCar.type[p] ) ;
+}
